f729c9ebba6f9c641452f6a627bd07738d3c6f10
shogoisaji <simacsimac9@gmail.com>
2023-11-22 10:38:33 +0900

    re change use client

--- app/components/list-card.tsx
+++ app/components/list-card.tsx
@@ -1,71 +1,81 @@
-import Image from "next/image";
-import Link from "next/link";
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
 import {
-  ArchiveBoxIcon,
-  ArchiveBoxXMarkIcon,
-} from "@heroicons/react/24/outline";
-import { Json } from "app/lib/database.types";
+    ArchiveBoxIcon,
+    ArchiveBoxXMarkIcon,
+} from '@heroicons/react/24/outline'
+import { Json } from 'app/lib/database.types'
 
 type Toy = {
-  created_at: string;
-  description: string | null;
-  id: string;
-  images: Json | null;
-  is_rented: boolean;
-  price: number;
-  product_name: string;
-};
+    created_at: string
+    description: string | null
+    id: string
+    images: Json | null
+    is_rented: boolean
+    price: number
+    product_name: string
+}
 
 type ToyCardProps = {
-  toy: Toy;
-};
+    toy: Toy
+}
 
 export const ListCard: React.FC<ToyCardProps> = ({ toy }) => {
-  const decodeImages = (images: Json | null): Array<string> | null => {
-    if (toy.images && typeof toy.images === "string") {
-      return JSON.parse(toy.images);
+    const decodeImages = (images: Json | null): Array<string> | null => {
+        if (toy.images && typeof toy.images === 'string') {
+            return JSON.parse(toy.images)
+        }
+        return ['/no_image.png']
     }
-    return ["/no_image.png"];
-  };
 
-  return (
-    <div className="shadow-xl shadow-black-900 max-w-sm rounded-xl bg-custom-blue5">
-      <Link href={`/toys/${toy.id}/detail`}>
-        <div
-          className="rounded-t-xl overflow-hidden"
-          style={{ width: "100%", height: "300px", position: "relative" }}
-        >
-          <Image
-            src={toy.images ? decodeImages(toy.images)![0] : "/no_image.png"}
-            alt={toy.product_name}
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
-      </Link>
-      <div className="flex flex-col justify-between p-5">
-        <div>
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
-            {toy.product_name}
-          </h5>
-          <p className="overflow-hidden h-20 mb-5 font-normal text-gray-300">
-            {toy.description}
-          </p>
-        </div>
-        <div className="flex justify-center items-center">
-          {toy.is_rented ? (
-            <ArchiveBoxXMarkIcon className="inline-block text-red-500 w-7 h-7 mr-3" />
-          ) : (
-            <ArchiveBoxIcon className="inline-block text-custom-blue7 w-7 h-7 mr-3" />
-          )}
+    return (
+        <div className="shadow-xl shadow-black-900 max-w-sm rounded-xl bg-custom-blue5">
+            <Link href={`/toys/${toy.id}/detail`}>
+                <div
+                    className="rounded-t-xl overflow-hidden"
+                    style={{
+                        width: '100%',
+                        height: '300px',
+                        position: 'relative',
+                    }}
+                >
+                    <Image
+                        src={
+                            toy.images
+                                ? decodeImages(toy.images)![0]
+                                : '/no_image.png'
+                        }
+                        alt={toy.product_name}
+                        layout="fill"
+                        objectFit="cover"
+                    />
+                </div>
+            </Link>
+            <div className="flex flex-col justify-between p-5">
+                <div>
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
+                        {toy.product_name}
+                    </h5>
+                    <p className="overflow-hidden h-20 mb-5 font-normal text-gray-300">
+                        {toy.description}
+                    </p>
+                </div>
+                <div className="flex justify-center items-center">
+                    {toy.is_rented ? (
+                        <ArchiveBoxXMarkIcon className="inline-block text-red-500 w-7 h-7 mr-3" />
+                    ) : (
+                        <ArchiveBoxIcon className="inline-block text-custom-blue7 w-7 h-7 mr-3" />
+                    )}
 
-          <div className="text-white">
-            <span className="text-white text-lg">￥</span>
-            {toy.price}
-            <span className="text-white text-sm"> / Day</span>
-          </div>
+                    <div className="text-white">
+                        <span className="text-white text-lg">￥</span>
+                        {toy.price}
+                        <span className="text-white text-sm"> / Day</span>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
--- app/components/rental.tsx
+++ app/components/rental.tsx
@@ -1,62 +1,65 @@
-"use client";
+'use client'
 
-import { useRouter } from "next/navigation";
-import Image from "next/image";
-import { useCallback, useEffect, useState } from "react";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { SubmitHandler } from "react-hook-form";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Database } from "app/lib/database.types";
-import useStore from "store";
+import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+import { useCallback, useEffect, useState } from 'react'
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { SubmitHandler } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Database } from 'app/lib/database.types'
+import useStore from 'store'
+import { Json } from 'app/lib/database.types'
 
 type Toy = {
-  created_at: string;
-  id: number;
-  image: string | null;
-  name: string;
-  description: string | null;
-};
+    created_at: string
+    description: string | null
+    id: string
+    images: Json | null
+    is_rented: boolean
+    price: number
+    product_name: string
+}
 
 const Rental = () => {
-  const router = useRouter();
-  const supabase = createClientComponentClient<Database>();
-  const [toysList, setToysList] = useState<Toy[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [message, setMessage] = useState("");
-  const { user } = useStore();
+    const router = useRouter()
+    const supabase = createClientComponentClient<Database>()
+    const [toysList, setToysList] = useState<Toy[]>([])
+    const [loading, setLoading] = useState(false)
+    const [message, setMessage] = useState('')
+    const { user } = useStore()
 
-  useEffect(() => {
-    getToys();
-  }, []);
+    useEffect(() => {
+        getToys()
+    }, [])
 
-  const getToys = async () => {
-    const { data: toys, error } = await supabase
-      .from("toys")
-      .select("*")
-      .order("id", { ascending: false });
-    if (error) {
-      setMessage(error.message);
-    } else {
-      setToysList(toys);
+    const getToys = async () => {
+        const { data: toys, error } = await supabase
+            .from('toys')
+            .select('*')
+            .order('id', { ascending: false })
+        if (error) {
+            setMessage(error.message)
+        } else {
+            setToysList(toys)
+        }
     }
-  };
 
-  return (
-    <div>
-      {toysList.map((toys) => (
-        <div key={toys.id}>
-          <h2>{toys.name}</h2>
-          <p>{toys.description}</p>
-          <Image
+    return (
+        <div>
+            {toysList.map((toys) => (
+                <div key={toys.id}>
+                    <h2>{toys.product_name}</h2>
+                    <p>{toys.description}</p>
+                    {/* <Image
             src={toys.image ? toys.image : "/avatars/avatar1.png"}
-            alt={toys.name}
+            alt={toys.product_name}
             width={100}
             height={100}
-          />
+          /> */}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
-};
-export default Rental;
+    )
+}
+export default Rental
